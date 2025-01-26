4c68d29b5fcc642d0967039e67fdaf81c9104685
Sandhyavr <92790350+Sandhyavr@users.noreply.github.com>
2025-01-26 17:07:57 +0530

    Update new functionality

--- src/sample/SampleClass.java
+++ src/sample/SampleClass.java
@@ -2,7 +2,8 @@ package sample;
 
 public class SampleClass {
     public static void main(String[] args){
-        System.out.println("Hi World");
+        System.out.println("Hi World changing for conflict");
         System.out.println("Added code on 26th Jan'25");
+        System.out.println("Adding new line for Conflict");
     }
 }
