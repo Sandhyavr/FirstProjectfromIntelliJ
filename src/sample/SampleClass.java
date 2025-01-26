ad2e6e7dad4c3f75f86cb1b7253bd480e6260ef3
Sandhyavr <92790350+Sandhyavr@users.noreply.github.com>
2025-01-26 17:14:00 +0530

    Update SampleClass.java

--- src/sample/SampleClass.java
+++ src/sample/SampleClass.java
@@ -2,7 +2,7 @@ package sample;
 
 public class SampleClass {
     public static void main(String[] args){
-        System.out.println("Hi World");
+        System.out.println("Hi World added by master");
         System.out.println("Added code on 26th Jan'25");
     }
 }
